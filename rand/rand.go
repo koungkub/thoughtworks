a073fe2a963021a338306520f1cc011c96c33f84
koungkub <1997jirasak@gmail.com>
2020-03-20 16:27:09 +0700

    🛩 add function random_hands

--- rand/rand.go
+++ rand/rand.go
@@ -1,6 +1,9 @@
 package rand
 
-import "math/rand"
+import (
+	"fmt"
+	"math/rand"
+)
 
 const (
 	// Max : max number can be expected
@@ -10,9 +13,20 @@ const (
 	Min = 0
 )
 
+// RandomHands : random hand if predictor true return `%s%s%d` pattern
+// if predictor is false return `%s%s` pattern
+func RandomHands(isPredictor bool) string {
+
+	if isPredictor {
+		return fmt.Sprintf("%v%v%v", RandomHand(RandomNumber(Max, Min)), RandomHand(RandomNumber(Max, Min)), RandomNumber(Max, Min))
+	}
+
+	return fmt.Sprintf("%v%v", RandomHand(RandomNumber(Max, Min)), RandomHand(RandomNumber(Max, Min)))
+}
+
 // RandomHand : random hand as 'O' is open, 'C' is close
 func RandomHand(seed int) string {
-	return []string{"O", "C"}[seed]
+	return []string{"O", "C", "O", "C"}[seed]
 }
 
 // RandomNumber : random number in range [min, max]
