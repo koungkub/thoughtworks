44f684796197b7b542910d7af172e6c72ad4ebc8
koungkub <1997jirasak@gmail.com>
2020-03-20 16:20:39 +0700

    😋 add function random_hand

--- rand/rand.go
+++ rand/rand.go
@@ -10,6 +10,11 @@ const (
 	Min = 0
 )
 
+// RandomHand : random hand as 'O' is open, 'C' is close
+func RandomHand(seed int) string {
+	return []string{"O", "C"}[seed]
+}
+
 // RandomNumber : random number in range [min, max]
 func RandomNumber(max, min int) int {
 	return min + rand.Intn(max-min)
