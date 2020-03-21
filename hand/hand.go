06c9d4657820839034128ee64d45a32432170150
koungkub <1997jirasak@gmail.com>
2020-03-21 19:21:24 +0700

    🏎 add function count_open

--- hand/hand.go
+++ hand/hand.go
@@ -3,7 +3,19 @@ package hand
 import (
 	"errors"
 	"regexp"
+	"strings"
 )
+
+// CountOpen : counter open ('O') in hand
+func CountOpen(patterns ...string) int {
+
+	var counter int
+	for _, pattern := range patterns {
+		counter += strings.Count(pattern, "O")
+	}
+
+	return counter
+}
 
 // Validate : validate pattern base on isPredictor flag
 func Validate(isPredictor bool, pattern string) error {
