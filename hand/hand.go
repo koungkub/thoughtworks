a3b2f74413f8b42531dc7cf9118d2bfdfe17fbd2
koungkub <1997jirasak@gmail.com>
2020-03-21 22:08:39 +0700

    add function compare

--- hand/hand.go
+++ hand/hand.go
@@ -3,15 +3,38 @@ package hand
 import (
 	"errors"
 	"regexp"
+	"strconv"
 	"strings"
 )
+
+const (
+
+	// Open : is represent 'O'
+	Open = `O`
+)
+
+// Compare : compare hand and check predictor is predic success or failure
+func Compare(predictor, notPredictor string) error {
+
+	ans, err := strconv.Atoi(string(predictor[2]))
+	if err != nil {
+		return err
+	}
+
+	openHand := CountOpen(predictor, notPredictor)
+	if openHand != ans {
+		return errors.New("predictor is invalid")
+	}
+
+	return nil
+}
 
 // CountOpen : counter open ('O') in hand
 func CountOpen(patterns ...string) int {
 
 	var counter int
 	for _, pattern := range patterns {
-		counter += strings.Count(pattern, "O")
+		counter += strings.Count(pattern, Open)
 	}
 
 	return counter
