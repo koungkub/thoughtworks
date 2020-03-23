df8af3d7edb06a4599ce7e6900c017378bd7183c
koungkub <1997jirasak@gmail.com>
2020-03-23 16:04:21 +0700

    🚎 add logic to play game

--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"fmt"
 	random "math/rand"
 	"time"
+
+	"github.com/koungkub/thw/rand"
+
+	"github.com/koungkub/thw/hand"
+	"github.com/koungkub/thw/io"
 )
 
 func init() {
@@ -11,4 +17,37 @@ func init() {
 
 func main() {
 
+	fmt.Println(io.Welcome())
+	predictorState := true
+
+	for {
+
+		fmt.Println(io.Turn(predictorState))
+
+		player := io.Scan("Player: ")
+		err := hand.Validate(predictorState, player)
+		if err != nil {
+			fmt.Println(err.Error())
+			continue
+		}
+
+		ai := rand.RandomHands(!predictorState)
+		fmt.Printf("AI: %v\n", ai)
+
+		err = hand.Compare(player, ai)
+		if err != nil {
+			fmt.Println("No winner !!")
+			predictorState = !predictorState
+			continue
+		}
+
+		confirm := io.Scan("Do you want to play again [Y]es or [N]o ?: ")
+		if confirm == "Y" || confirm == "YES" {
+			predictorState = !predictorState
+			continue
+		}
+
+		fmt.Println("OK, bye !!")
+		break
+	}
 }
